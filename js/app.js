042c03be3549bd7b184e9d6b555c6a4013ec75e4
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-10-22 20:46:57 +1100

    Add functions to create header and sections

--- js/app.js
+++ js/app.js
@@ -12,7 +12,49 @@
  * JS Standard: ESlint
  *
  */
-console.log("Test");
+
+setHeader = (header) => {
+  const headerMain = document.querySelector(".main__hero");
+  const headerMainText = document.createElement("h1");
+  headerMain.appendChild(headerMainText).textContent = header;
+};
+
+setHeader("Natalia's page");
+
+const containers = document.querySelectorAll(".landing__container");
+setSection = (cont, header, a, b) => {
+  const sectionHeader = document.createElement("h2");
+  const sectionParagraphOne = document.createElement("p");
+  const sectionParagraphTwo = document.createElement("p");
+
+  cont.appendChild(sectionHeader).textContent = header;
+  cont.appendChild(sectionParagraphOne).textContent = a;
+  cont.appendChild(sectionParagraphTwo).textContent = b;
+};
+
+setSection(
+  containers[0],
+  "This is Section 1",
+  "This is the first paragraph with some information in it.",
+  "This is the second paragraph with some information in it."
+);
+
+setSection(
+  containers[1],
+  "This is Section 2",
+  "This is the first paragraph with some information in it.",
+  "This is the second paragraph with some information in it."
+);
+
+setSection(
+  containers[2],
+  "This is Section 3",
+  "This is the first paragraph with some information in it.",
+  "This is the second paragraph with some information in it."
+);
+
+console.log(containers);
+
 /**
  * Define Global Variables
  *
