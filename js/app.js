f08e6200dc21d581ae3416b6eea720e7b329b56f
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-10-23 19:32:51 +1100

    Add text content, function to build sections and navBar

--- js/app.js
+++ js/app.js
@@ -13,58 +13,80 @@
  *
  */
 
-setHeader = (header) => {
-  const headerMain = document.querySelector(".main__hero");
-  const headerMainText = document.createElement("h1");
-  headerMain.appendChild(headerMainText).textContent = header;
-};
-
-setHeader("Natalia's page");
-
-const containers = document.querySelectorAll(".landing__container");
-setSection = (cont, header, a, b) => {
-  const sectionHeader = document.createElement("h2");
-  const sectionParagraphOne = document.createElement("p");
-  const sectionParagraphTwo = document.createElement("p");
-
-  cont.appendChild(sectionHeader).textContent = header;
-  cont.appendChild(sectionParagraphOne).textContent = a;
-  cont.appendChild(sectionParagraphTwo).textContent = b;
-};
-
-setSection(
-  containers[0],
-  "This is Section 1",
-  "This is the first paragraph with some information in it.",
-  "This is the second paragraph with some information in it."
-);
-
-setSection(
-  containers[1],
-  "This is Section 2",
-  "This is the first paragraph with some information in it.",
-  "This is the second paragraph with some information in it."
-);
-
-setSection(
-  containers[2],
-  "This is Section 3",
-  "This is the first paragraph with some information in it.",
-  "This is the second paragraph with some information in it."
-);
-
-console.log(containers);
-
 /**
  * Define Global Variables
  *
  */
+
+const sectionsData = [
+  {
+    header: "About me",
+    p1:
+      "My name is Natalia Romaniuk. Two years ago I’ve changed country and profession. I moved with my family from Russia to Australia and started to learn web development.",
+    p2:
+      "Studying programming from scratch is quite challenging but inspiring. JavaScript, React, Redux, CSS, HTMP, Bootstrap now don’t sound weird to me, these guys became part of my life. I keep studying and looking for web developer job.",
+  },
+  {
+    header: "Experience",
+    p1:
+      "Currently I'm working on my personal projects, kids-camps.club is one of them. During 2019 I was working as a coding teacher for kids 7-12 years old. I was teaching them how to create games and cartoons using Scratch - a block-based visual programming language.",
+    p2:
+      "I also have an experience in working with content for company's website. I was creating appropriate website content according to company’s strategy.",
+  },
+  {
+    header: "Education",
+    p1:
+      "I've completed courses: JavaScript developer, The ultimate Redux course, React course - ​building and launching React web applications using React, Redux, Webpack, React-Router.",
+    p2:
+      "I have bachelor​d​ egree in managment. Public relations manager in high technology enterprises.",
+  },
+];
+
+const sections = document.querySelectorAll(".landing__container");
+const navBar = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+const setNavigation = () => {
+  sectionsData.forEach((section) => {
+    const navItem = document.createElement("li");
+    navBar.appendChild(navItem).textContent = section.header;
+  });
+};
+
+const setHeader = (header) => {
+  const headerMain = document.querySelector(".main__hero");
+  const headerMainText = document.createElement("h1");
+  headerMain.appendChild(headerMainText).textContent = header;
+};
+
+const setSection = (cont, header, p1, p2) => {
+  const sectionHeader = document.createElement("h2");
+  const sectionParagraphOne = document.createElement("p");
+  const sectionParagraphTwo = document.createElement("p");
+
+  cont.appendChild(sectionHeader).textContent = header;
+  cont.appendChild(sectionParagraphOne).textContent = p1;
+  cont.appendChild(sectionParagraphTwo).textContent = p2;
+};
+
+const biuldSections = () => {
+  sections.forEach((section, index) => {
+    setSection(
+      section,
+      sectionsData[index].header,
+      sectionsData[index].p1,
+      sectionsData[index].p2
+    );
+  });
+};
+
+setNavigation();
+setHeader("Natalia's CV page");
+biuldSections();
 
 /**
  * End Helper Functions
